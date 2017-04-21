1ab57cf6c1ae1e77d5fd4fe3fbfb0ed01c7f9755
Shadowsky <michallo.sadek@gmail.com>
2017-04-21 21:32:27 +0200

    first commit

---
+++ LeagueMatches/game/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from game.models import *
+
+
+admin.site.register(Serie)
+admin.site.register(Team)
+admin.site.register(Game)
+admin.site.register(MapObjective)
+admin.site.register(Event)
+admin.site.register(Player)
---
+++ LeagueMatches/game/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+class Serie(models.Model):
+    begin = models.DateField()
+    end = models.DateField()
+
+    def __str__(self):
+        return str(self.begin) + ' - ' + str(self.end)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    date = models.DateField()
+    teamA = models.ForeignKey(Team, related_name='teamA')
+    teamB = models.ForeignKey(Team, related_name='teamB')
+    winner = models.ForeignKey(Team, related_name='winner')
+    serie = models.ForeignKey(Serie)
+
+    def __str__(self):
+        return self.teamA.name + ' vs ' + self.teamB.name + ' ' + str(self.date)
+
+
+class MapObjective(models.Model):
+    type = models.CharField(max_length=20)
+    value = models.FloatField()
+
+    def __str__(self):
+        return self.type
+
+
+class Event(models.Model):
+    time = models.DateTimeField()
+    game = models.ForeignKey(Game)
+    mapObjective = models.ForeignKey(MapObjective)
+
+    def __str__(self):
+        return self.mapObjective.type + ' ' + str(self.time)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=20)
+    team = models.ForeignKey(Team)
+
+    def __str__(self):
+        return self.nickname
