c1d3eca45fd84f99eb03852e7a54ebd4031fdcd2
NotMeBtw <matyszxzx@gmail.com>
2017-06-06 22:41:21 +0200

    Captcha

--- LeagueMatches/managePanel/urls.py
+++ LeagueMatches/managePanel/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 app_name = 'manage'
 
-
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^login/$', auth_views.login, {'template_name': '/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
--- LeagueMatches/managePanel/views.py
+++ LeagueMatches/managePanel/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
     return render_to_response('login.html')
+
+# COS TU KOMBINOWALEM, ALE JESZCZE NIE DZIALA
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            render_to_response('udalosie.html')
+        else:
+            print "disabled account"
+    else:
+       print "invalid login"
+
+
+def logout_view(request):
+    logout(request)
